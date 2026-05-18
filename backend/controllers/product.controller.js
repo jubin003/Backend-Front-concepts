12dfa809aafcb9ce723ffcb82aaa2a1293e1e5eb
jubin003 <jubin4351599@gmail.com>
2026-05-18 21:04:05 +0545

    backend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,6 +1,14 @@
-const getProduct = async(req,res()=>{
+const Product= require('./models/product.model.js');
 
-});
+const getProduct = async(req,res)=>{
+    try{
+        const product= await Product.find();
+        res.status(200).json(product);
+    }catch(error){
+        res.status(500).json({mesasge:error.mesasge})
+    }
+
+};
 
 module.exports={
     getProduct
--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const productRoute= require('./routes/product.route.js');
 
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
 app.use('/api/products',productRoute);
 
